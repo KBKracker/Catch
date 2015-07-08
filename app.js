f1d5080a554c21007aa27e40a388562b92b3b05f
KBKracker <student8@js-c02mxpsdg083.punahou.edu>
2015-07-08 10:37:03 -1000

    All the load

---
+++ app.js
@@ -0,0 +1,20 @@
+ $(document).ready(function(){
+    $("button").click(function(){
+        $("div").animate({left: '250px'});
+    });
+});
+$(document).ready(function(){
+    $("button").click(function(){
+        $("div").animate({down: '250px'});
+    });
+}); 
+$(document).ready(function(){
+    $("button").click(function(){
+        $("div").animate({right: '250px'});
+    });
+}); 
+$(document).ready(function(){
+    $("button").click(function(){
+        $("div").animate({up: '250px'});
+    });
+}); 
