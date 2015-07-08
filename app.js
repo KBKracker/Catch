a8710342354cec721399cf16673100532017a4b6
KBKracker <student8@js-c02mxpsdg083.punahou.edu>
2015-07-08 10:45:54 -1000

    LOAD

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
- $(document).ready(function(){
+$(document).ready(function(){
     $("button").click(function(){
-        $("div").animate({left: '250px'});
+        $("div").animate({
+            left: '250px',
+            
+        });
     });
 });
-$(document).ready(function(){
-    $("button").click(function(){
-        $("div").animate({down: '250px'});
-    });
-}); 
-$(document).ready(function(){
-    $("button").click(function(){
-        $("div").animate({right: '250px'});
-    });
-}); 
-$(document).ready(function(){
-    $("button").click(function(){
-        $("div").animate({up: '250px'});
-    });
-}); 
