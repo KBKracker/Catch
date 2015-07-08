c7d42f899f0ad75ba2faf38d7305c04093fd69a1
KBKracker <student8@js-c02mxpsdg083.punahou.edu>
2015-07-08 11:21:08 -1000

    Loadable

--- app.js
+++ app.js
@@ -1,36 +1,11 @@
-<<<<<<< Updated upstream
+$("button").click(function(){
+    alert("The game is now loading");
+    setTimeout(function(){ 
+        alert("Loaded"); 
+            $("button").hide(); 
+            $("h2").hide();
+    }, 60000);
 
-$(document).ready(function(){
-    $("button").click(function(){
-        $("div").animate({
-            left: '250px',
-            
-        });
-    });
 });
-var Uscore = 1;
 
-var highscore = 10;
 
-var press = function () {
-	Uscore++;
-	document.getElementById('score').innerHTML = "Your Score: " + Uscore;
-	if (Uscore > highscore) {
-		document.getElementById('hiscore').innerHTML = "New Highscore: " + Uscore;
-		return;
-		}
-};
-=======
-$("button").click(function(){
-    $("#box").animate({height: "300px"});
-});
-$("button").click(function(){
-    $("#box").animate({width: "300px"});
-});
-$("button").click(function(){
-    $("#box").animate({height: "150px"});
-});
-$("button").click(function(){
-    $("#box").animate({height: "150px"});
-});
->>>>>>> Stashed changes
