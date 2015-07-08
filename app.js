e52a343e8a54408b3fc0ede4b78f8be14beadddb
JJJosh21 <jwong21@punahou.edu>
2015-07-08 11:26:34 -1000

    Update app.js

--- app.js
+++ app.js
@@ -8,4 +8,17 @@ $("button").click(function(){
 
 });
 
+var Uscore = 1;
+
+var highscore = 10;
+
+var press = function () {
+	Uscore++;
+	document.getElementById('score').innerHTML = "Your Score: " + Uscore;
+	if (Uscore > highscore) {
+		document.getElementById('hiscore').innerHTML = "New Highscore: " + Uscore;
+		return;
+		}
+};
+
 
