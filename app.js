f42a92045228603bfeb128ba03a2334cce95b582
KBKracker <student8@js-c02mxpsdg083.punahou.edu>
2015-07-09 11:52:37 -1000

    WORKS

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
+
+var Uscore = 0;
+var highscore = 10;
 $("button").click(function(){
+
     alert("The game is now loading");
+    $("h3").hide();
+    $("h1").hide();
+    $("p").hide();
     setTimeout(function(){ 
         alert("Loaded"); 
             $("button").hide(); 
             $("h2").hide();
-    }, 60000);
-     window.alert("Validation May Take A While");
+            $("h1").show();
+            $("h3").show();
+            $("p").show();
+            $("h1").css("color","black");
+            $("h3").css("color","black");
+
+			$("#clicky").click(function() {
+				Uscore++;
+				$('#ranson').html('<p>' + Uscore + '</p>');
+				$("p").css("color","black");
+// document.getElementById('score').innerHTML = "Your Score:" + Uscore;
+				if (Uscore > highscore) {
+					highscore = Uscore;
+					$('#highscore').html("New Highscore " + Uscore);
+				}
+			});
+    }, 1000);
    });
 
