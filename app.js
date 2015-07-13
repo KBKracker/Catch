f6949bfe5c6f4626b908ec61df8d36cb8c029ae9
KBKracker <student8@js-c02mxpsdg083.punahou.edu>
2015-07-13 10:55:21 -1000

    FINAL

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
-
+//rip Iwata
 var Uscore = 0;
 var highscore = 10;
-$("button").click(function(){
+var s = 1;
+$("#juice").hide();
+$("#loose").click(function(){
 
     alert("The game is now loading");
     $("h3").hide();
     $("h1").hide();
     $("p").hide();
+    $("i").hide();
     setTimeout(function(){ 
         alert("Loaded"); 
-            $("button").hide(); 
+            $("#loose").hide();
             $("h2").hide();
             $("h1").show();
             $("h3").show();
             $("p").show();
+            $("i").show();
             $("h1").css("color","black");
             $("h3").css("color","black");
+            $("i").css("color","black");
 
 			$("#clicky").click(function() {
-				Uscore++;
+				Uscore+=s;
 				$('#ranson').html('<p>' + Uscore + '</p>');
 				$("p").css("color","black");
 // document.getElementById('score').innerHTML = "Your Score:" + Uscore;
@@ -27,6 +32,18 @@ $("button").click(function(){
 					$('#highscore').html("New Highscore " + Uscore);
 				}
 			});
-    }, 1000);
+    }, 5000);
    });
+
+setTimeout(function(){
+        $("#juice").show();
+}, 15000);
+
+$("#juice").click(function(){
+    alert("Your upgrade is being confirmed");
+    setTimeout(function(){
+        alert("You now have a x2 upgrade");
+        s=2;
+    }, 15000);
+});
 
